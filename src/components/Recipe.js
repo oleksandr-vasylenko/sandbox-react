e53c77a5c6532dee4da6fa8713a05e9f17838922
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-26 20:18:49 +0200

    Renders cards

---
+++ src/components/Recipe.js
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import { BsAlarm } from 'react-icons/bs';
+import { HiOutlineChartPie, HiOutlineChartBar } from 'react-icons/hi';
+
+export const Recipe = ({ recipe: { name, time, servings, calories } }) => {
+  return (
+    <div style={{ border: '1px solid black' }}>
+      <h2>{name}</h2>
+      <div>
+        <div>
+          <BsAlarm />
+          {time} mins
+        </div>
+        <div>
+          <HiOutlineChartPie />
+          {servings} servings
+        </div>
+        <div>
+          <HiOutlineChartBar />
+          {calories} calories
+        </div>
+      </div>
+      <div>
+        <h3>Difficulty</h3>
+        <div>
+          <span>Easy</span>
+          <span>Medium</span>
+          <span>Hard</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Recipe.propTypes = {
+  recipe: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    serving: PropTypes.number.isRequired,
+    calories: PropTypes.number.isRequired,
+  }).isRequired,
+};
